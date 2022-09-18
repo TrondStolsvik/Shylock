1ffe335696301516b06d381d8881651128051a73
TrondStolsvik <trondstolsvik@gmail.com>
2022-09-18 20:21:16 +0200

    working on pseudo code

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
-function HelloWorld() {
-  alert("Hello World!");
+//Get the values from the page
+function getValues() {
+  //Get the value from loan amount and store it in a variable
+  //Get the value from term and store it in a variable
+  //Get the value from interest rate and store it in a variable
+  //
+}
+
+//Calculate montly information
+function monthlyPayments() {
+  //
+}
+
+//Display the header totals and table
+function displayLoan() {
+  //
 }
